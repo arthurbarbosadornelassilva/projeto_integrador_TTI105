c8d23fd9d82897e9491bbc8b383d14da96cff76a
ARTHUR BARBOSA DORNELAS SILVA <24.00813-3@maua.br>
2024-05-07 08:00:15 -0300

    acréscimo design primeiro maze

--- modelo_maze01.py
+++ modelo_maze01.py
@@ -7,10 +7,10 @@ pygame.display.set_caption('Vac-Man.exe')
 
 larguraTela = 960
 alturaTela = 680
-x = 60
-y = 100
+x = 30
+y = 30
 tela = pygame.display.set_mode((larguraTela, alturaTela))
-plano_de_fundo = pygame.image.load('img\Maze.png')
+plano_de_fundo = pygame.image.load('img\Fundo1.1.png')
 relogio = pygame.time.Clock()
 
 while True:
@@ -23,44 +23,45 @@ while True:
     #Em LIMITETELA a configuração dos limites das bordas era feita de maneira diferente desta aqui
     #Além disso, aqui é possível utilizar o 'get_at' como identificador de cor e limitador da tela
     keys = pygame.key.get_pressed()
-    lado_x = 70
-    lado_y = 70
-    cor_padrão = (0, 0, 0, 255)
+    lado_x = 40
+    lado_y = 40
+    cor_padrão = (255, 174, 201, 255)
 
     if keys[pygame.K_LEFT] and x > 5:
         x -=5
         cor_cima = tela.get_at((x - 1, y))
-        cor_baixo = tela.get_at((x - 1, y + 70))
+        cor_baixo = tela.get_at((x - 1, y + 40))
         if cor_cima != cor_padrão or cor_baixo != cor_padrão:
             x += 5
     
     if keys[pygame.K_RIGHT] and x < 960 -lado_x -5:
         x += 5
-        cor_cima = tela.get_at((x + 70 , y))
-        cor_baixo = tela.get_at((x + 70, y + 70))
+        cor_cima = tela.get_at((x + 40 , y))
+        cor_baixo = tela.get_at((x + 40, y + 40))
         if cor_cima != cor_padrão or cor_baixo != cor_padrão:
             x -= 5
+
 
     if keys[pygame.K_UP] and y > 5:
         y -= 5
         cor_cima = tela.get_at((x, y - 1))
-        cor_baixo = tela.get_at((x + 70, y -1))
+        cor_baixo = tela.get_at((x + 40, y -1))
         if cor_cima != cor_padrão or cor_baixo != cor_padrão:
             y += 5
 
     if keys[pygame.K_DOWN] and y < 720 -lado_y -5:
         y += 5
-        cor_cima = tela.get_at((x, y + 70))
-        cor_baixo = tela.get_at((x + 70, y + 70))
+        cor_cima = tela.get_at((x, y + 40))
+        cor_baixo = tela.get_at((x + 40, y + 40))
         if cor_cima != cor_padrão or cor_baixo != cor_padrão:
             y -= 5
 
     tela.fill((0,0,0))
     tela.blit(plano_de_fundo, (0, 0))
-    protagonista = pygame.draw.rect(tela, (0, 255, 255), (x, y, lado_x, lado_y))
+    protagonista = pygame.draw.rect(tela, (255, 255, 200), (x, y, lado_x, lado_y))
 
     #definindo o retângulo para passar de fase:
-    proxFase = pygame.draw.rect(tela, (255, 0, 0), (900, 550, 40, 70))
+    proxFase = pygame.draw.rect(tela, (255, 0, 0), (900, 550, 40, 40))
     if protagonista.colliderect(proxFase):
         pygame.quit()
         exit()
